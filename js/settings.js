7c1139e92de067962e77d3ca24b81bcbaab1b8c0
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-21 17:52:08 +0300

    bug fixes

--- js/settings.js
+++ js/settings.js
@@ -6,13 +6,14 @@ var maxSuggestionResults = 3;
 var videoLoadingDelayTolerance = 4;
 
 var spinner_opts = {
-  lines: 8, // The number of lines to draw
+  lines: 6, // The number of lines to draw
   length: 0, // The length of each line
   width: 4, // The line thickness
-  radius: 10, // The radius of the inner circle
+  radius: 4, // The radius of the inner circle
   color: '#000', // #rgb or #rrggbb
   speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
+  trail: 100, // Afterglow percentage
   shadow: false // Whether to render a shadow
 };
 var spinner_target = document.getElementById('suggestionLoading');
+var spinner_target_2 = document.getElementById('searchLoading');
