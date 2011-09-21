681229fc8610d088ecd06d97c5f2ec789fc8f8f1
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-21 18:25:13 +0300

    Revert 9804a93fa8e631c72f7b1fb29c056a4eebd12011^..HEAD

--- js/settings.js
+++ js/settings.js
@@ -4,16 +4,3 @@ var lastFmApiKey = "6a040e6842f5c100c5a9816c4e28cdaf"; // enter your lastfm api 
 var maxSearchResults = 5;
 var maxSuggestionResults = 3;
 var videoLoadingDelayTolerance = 4;
-
-var spinner_opts = {
-  lines: 6, // The number of lines to draw
-  length: 0, // The length of each line
-  width: 4, // The line thickness
-  radius: 4, // The radius of the inner circle
-  color: '#000', // #rgb or #rrggbb
-  speed: 1, // Rounds per second
-  trail: 100, // Afterglow percentage
-  shadow: false // Whether to render a shadow
-};
-var spinner_target = document.getElementById('suggestionLoading');
-var spinner_target_2 = document.getElementById('searchLoading');
