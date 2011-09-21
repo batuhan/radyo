aa7127c97badf6eed56e5635f09b7ef2924e86e2
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-21 14:53:59 +0300

    youtube iframe api, some bug fixes, cosmetic changes

---
+++ app.js
@@ -0,0 +1,272 @@
+var lastFmApiKey = "6a040e6842f5c100c5a9816c4e28cdaf";
+var maxSearchResults = 5;
+var maxSuggestionResults = 3;
+var videoLoadingDelayTolerance = 4;
+
+var searchResults, suggestionResults;
+var playedSoFar = new Array();
+
+var searchTimeout;
+
+var curDuration, curElapsed;
+//var timer;
+
+function timerFunction(){
+	if(curElapsed <= curDuration) $('#time').html(strSecs(curElapsed) + '/' + strSecs(curDuration));
+	if (curElapsed > curDuration + videoLoadingDelayTolerance){
+		if (timer) {
+			clearInterval(timer);
+		}
+		playNext();
+	} else {
+		curElapsed += 1;
+	}
+}
+
+function strSecs(secs){
+	return Math.floor(secs/60)+':'+secs%60;
+}
+
+function playNext(){
+	if (suggestionResults){
+		nextTrack = suggestionResults.shift();
+		if (nextTrack){
+			startPlaying(nextTrack);
+		}
+	}
+}
+
+function doSearch(q){
+	$('#searchResults').html('');
+	preSearchHook();
+
+	url = 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+encodeURIComponent(q)+'&api_key='+ lastFmApiKey;
+
+	$.get(url, function(xml){
+
+
+		$('track', xml).each(function(i){
+			var images = new Array();
+
+			$(this).find("image").each(function(img){
+				images.push($(this).text());
+			});
+
+			searchResults.push({
+				name: $(this).find("name").text(),
+				artist: $(this).find("artist").text(),
+				images: images
+			});
+		});
+
+		postSearchHook();
+	});
+}
+
+function preSearchHook(){
+	$('#searchLoading').show();
+	$('#searchResults').html('');
+	searchResults = new Array();
+	$('#searchResults').slideDown();
+}
+
+function postSearchHook(){
+	$('#searchLoading').hide();
+	searchResults = searchResults.slice(0, maxSearchResults);
+
+	if (searchResults.length == 0){
+		$('#searchResults').html('No results found.');
+	} else {
+		searchResults.map(function(result){
+			main = $('<div />', {
+				class: 'result',
+				click: function() {
+						startPlaying(result); 	
+						$('#searchResults').slideUp();
+						return false;
+					}
+			}).appendTo('#searchResults')
+
+			anchor = $('<a />', {
+				src: result.images[0], // small
+				text: result.artist + ' - ' + result.name,
+				href: '#'
+			}).appendTo(main);
+
+			$('<img />', {
+				src: result.images[0], // small
+				width: 30,
+				height: 30,
+				class: 'albumart'					
+			}).prependTo(anchor);
+
+		});
+
+		$('#searchResults').slideDown();
+	}
+}
+
+function suggestTracks(artist, title){
+	url= 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist='+ artist +'&track='+ title +'&autocorrect=1&api_key='+ lastFmApiKey
+
+	preSuggestionHook();		
+
+	$.get(url, function(xml){
+		$('track', xml).each(function(i){
+
+			var _images = new Array();
+
+			$(this).find("image").each(function(img){
+				_images.push($(this).text());
+			});
+
+			suggestionResults.push({
+				name: $(this).find("name").first().text(),
+				artist: $(this).find("artist").find("name").text(),
+				images: _images
+			});
+		});
+
+		postSuggestionHook();
+	});
+}
+
+function preSuggestionHook(){
+	$('#suggestionResults').html('');
+	suggestionResults = new Array();
+	$('#suggestionLoading').show();
+}
+
+function postSuggestionHook(){
+	$('#suggestionLoading').hide();
+	$('#suggestions').slideDown();
+
+	filteredResults = new Array();
+	suggestionResults.map(function(track){
+		if (playedSoFar.indexOf(track.name) == -1){
+			filteredResults.push(track);
+		}
+	});
+
+	suggestionResults = filteredResults;
+
+	if(suggestionResults.length < 1) $('#btnNext').attr('disabled', 'disabled');
+	else $('#btnNext').removeAttr('disabled');
+
+	renderSuggestionResults(suggestionResults);
+}
+
+function renderSuggestionResults(results){
+	results = results.slice(0, maxSuggestionResults);
+
+	$('#suggestionResults').empty();
+
+	if (results.length == 0){
+		$('#suggestionResults').html('No suggestions to play next. Please search for more famous tracks.');
+	} else {
+		results.map(function(result){
+			main = $('<div />', {
+				class: 'result'
+			}).appendTo('#suggestionResults');
+
+			meta = $('<a />', {
+				class: 'meta',
+				href: '#',
+				click: function(){
+					$('#suggestions').slideUp(
+						function(){
+							startPlaying(result);
+							return false;
+						});
+				}
+			}).appendTo(main);
+
+			$('<div />', {
+				class: 'title',
+				text: result.name
+			}).appendTo(meta);
+
+			$('<div />', {
+				class: 'artist',
+				text: result.artist
+			}).appendTo(meta);
+
+			$('<a />', {
+				class: 'actions',
+				text: "✖ Skip",
+				click: function(){
+					$(this).parent().remove();
+					if (suggestionResults){
+						var tmp = suggestionResults.shift();
+						//suggestionResults.push(tmp);
+						renderSuggestionResults(suggestionResults);
+					}
+				}
+			}).appendTo(main);
+
+			$('<img />', {
+				src: result.images[1], // med
+				width: 64,
+				height: 64,
+				class: 'albumart'					
+			}).prependTo(
+				meta
+			);
+
+			$('#suggestionResults .actions').hide().first().show();
+		});
+	}
+}
+
+function startPlaying(result){
+	if (result.images){
+		_biggestImg = result.images.pop();
+		if (_biggestImg) $('#albumCover').attr('src', _biggestImg); 
+	}
+	
+	startPlayingFromYoutube(result.artist, result.name, result.artist + ' ' + result.name);
+}
+
+function startPlayingFromYoutube(artist, title, songname){		
+	
+	playedSoFar.push(title);
+	
+	currentIndex = 0;
+    videos = [];
+
+    getFeed(player, songname, 1);loadSong
+
+// show info and now playing info.
+	$('#nowPlaying').html(artist+' - '+title);
+	$('#info').show();
+
+	suggestTracks(artist,title);
+
+    return false;
+
+}
+
+// on document ready
+$(function(){		
+	$('#q').keyup(function(){
+		$('#searchLoading').show();
+		if (searchTimeout) clearTimeout(searchTimeout);
+
+		searchTimeout = setTimeout(function(){
+			if ($('#q').val())
+				doSearch($('#q').val());
+			else 
+				$('#searchLoading').hide();
+		}, 500);
+	});
+
+	$('#q').focus(function(){
+		if ($(this).val()=='Type an artist or song name')
+			$(this).val('');
+	})
+
+	$('#q').blur(function(){
+		if (!$(this).val()) $(this).val('Type an artist or song name');
+	})
+
+});
---
+++ youtube.js
@@ -0,0 +1,237 @@
+var seekBarInterval = null;
+var currentIndex = 0;
+var videos = [];
+
+var DEFAULT_VIDEO_ID = '';
+var URL_PREFIX = 'http://gdata.youtube.com/feeds/api/videos?q=';
+var MAX_RESULTS = 1;
+var QUERY_PARAMS = 'v=2&format=5&alt=json&fields=entry(title,media:group(media:thumbnail' +
+  '[@yt:name="default"](@url),yt:videoid))&max-results=' + MAX_RESULTS;
+
+$(document).ready(function() {
+  
+  $('#playerDiv').hide();
+  
+  disable('previous', 'pause', 'play', 'next', 'volume', 'seek');
+  
+  var tag = document.createElement('script');
+  tag.src = 'http://www.youtube.com/player_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
+
+function getFeed(player, songName, startIndex) {
+  currentIndex = 0;
+  
+  $.ajax({
+    dataType: 'jsonp',
+    url: URL_PREFIX + songName + '&' + QUERY_PARAMS + '&start-index=' + startIndex,
+    success: function(response) {
+      if (response.feed && response.feed.entry) {
+        $.each(response.feed.entry, function(index, entry) {
+          videos.push({
+            'title': entry['title']['$t'],
+            'thumbnail': entry['media$group']['media$thumbnail'][0]['url'],
+            'videoid': entry['media$group']['yt$videoid']['$t'],
+          });
+        });
+        
+          playCurrentVideo(player);
+        
+      } else {
+        playCurrentVideo(player);
+      }
+    }
+  });
+}
+
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: DEFAULT_VIDEO_ID,
+    playerVars: {
+      'controls': 0,
+      'enablejsapi': 1,
+      'html5': 1,
+      'origin': window.location.host
+    },
+    events: {
+      'onReady': onReady,
+      'onStateChange': onStateChange,
+      'onPlaybackQualityChange': onPlaybackQualityChange,
+      'onError': onError
+    }
+  });
+  
+  $('#pause').click(function() {
+    if ($(this).attr('disabled') == 'false') {
+      player.pauseVideo();
+    }
+  });
+  
+  $('#play').click(function() {
+    if ($(this).attr('disabled') == 'false') {
+      player.playVideo();
+    }
+  });
+  
+  $('#volume').change(function() {
+    player.setVolume($(this).val());
+  });
+  
+  $('#seek').change(function() {
+    if (seekBarInterval != null) {
+      clearInterval(seekBarInterval);
+    }
+    
+    $('#currentTime').html(secondsToMmSs($(this).val()));
+  });
+  
+  $('#seek').mouseup(function() {
+    player.seekTo($(this).val(), true);
+    
+    setSeekBarInterval();
+  });
+  
+  $('#loadSong').click(function() {
+    currentIndex = 0;
+    videos = [];
+    
+    getFeed(player, $('#songName').val(), 1);loadSong
+
+    return false;
+  });
+}
+
+function playCurrentVideo(player) {
+  if (seekBarInterval != null) {
+    clearInterval(seekBarInterval);
+  }
+  
+  $('#currentTime').html(secondsToMmSs(0));
+  $('#duration').html(secondsToMmSs(0));
+  
+  if (videos.length > 0) {
+    $('#playerDiv').show();
+  
+    var currentVideo = videos[currentIndex];
+  
+    if (currentVideo != null) {
+      $('#title').html(currentVideo.title);
+      player.loadVideoById(currentVideo.videoid);
+      
+      enable('play');
+      disable('pause');
+    }
+  } else {
+    $('#playerDiv').hide();
+  }
+}
+
+function onReady(event) {
+  enable('loadSong');
+}
+
+function onStateChange(event) {
+  console.log('State is ' + event.data);
+  
+  var player = event.target;
+
+  switch (event.data) {
+    case YT.PlayerState.ENDED:
+      if (seekBarInterval != null) {
+        clearInterval(seekBarInterval);
+        seekBarInterval = null;
+      }
+      
+      var duration = Math.round(player.getDuration());
+      $('#currentTime').html(secondsToMmSs(duration));
+      $('#seek').val(duration);
+
+      enable('play');
+      disable('pause', 'volume', 'seek');
+      
+      playNextVideo(player);
+    break;
+      
+    case YT.PlayerState.PLAYING:
+      if (seekBarInterval != null) {
+        clearInterval(seekBarInterval);
+      }
+      
+      setSeekBarInterval();
+      
+      enable('pause', 'volume', 'seek');
+      disable('play');
+      
+      $('#volume').val(player.getVolume());
+      
+      var duration = Math.round(player.getDuration());
+      $('#duration').html(secondsToMmSs(duration));
+      $('#seek').attr('max', duration);
+    break;
+      
+    case YT.PlayerState.PAUSED:
+      enable('play', 'volume', 'seek');
+      disable('pause');
+      
+      if (seekBarInterval != null) {
+        clearInterval(seekBarInterval);
+        seekBarInterval = null;
+      }
+    break;
+      
+    case YT.PlayerState.BUFFERING:
+      enable('pause', 'volume', 'seek');
+      disable('play');
+      
+      if (seekBarInterval != null) {
+        clearInterval(seekBarInterval);
+        seekBarInterval = null;
+      }
+    break;
+      
+    case YT.PlayerState.CUED:
+      enable('play');
+      disable('pause', 'volume', 'seek');
+    break;
+  }
+}
+
+function enable() {
+  $.each(arguments, function(i, id) {
+    $('#' + id).attr('disabled', false);
+  });
+}
+
+function disable() {
+  $.each(arguments, function(i, id) {
+    $('#' + id).attr('disabled', true);
+  });
+}
+
+function secondsToMmSs(seconds) {
+  var minutesValue = Math.floor(seconds / 60);
+  var secondsValue = Math.floor(seconds % 60);
+  if (secondsValue < 10) {
+    secondsValue = '0' + secondsValue;
+  }
+  
+  return minutesValue + ':' + secondsValue;
+}
+
+function setSeekBarInterval() {
+  seekBarInterval = setInterval(function() {
+    var currentTime = Math.round(player.getCurrentTime());
+    $('#currentTime').html(secondsToMmSs(currentTime));
+    $('#seek').val(currentTime);
+  }, 1000);
+}
+
+function onPlaybackQualityChange(event) {
+}
+
+function onError(event) {
+  console.log(event);
+}
